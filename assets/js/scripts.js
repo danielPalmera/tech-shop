556efc761776ef5158517aa956917a2761b705ba
danielPalmera <daniel@palmera.marketing>
2026-03-11 01:11:31 +0100

    Actualizacion de el header, se agrego se agrego el menu lateral de las categorias se centro el copie en el main

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,11 +14,12 @@ menuLinks.forEach(link => {
 /*
 * funcion de cerrar 
 */ 
-
 const closeSidebar = () => {
     sidebar.classList.remove('active');
     overlay.classList.remove('active');
 };
+
+
 
 closeBtn.addEventListener('click', closeSidebar);
 overlay.addEventListener('click', closeSidebar);
@@ -26,4 +27,32 @@ overlay.addEventListener('click', closeSidebar);
 openBtn.addEventListener('click', () => {
     sidebar.classList.add('active');
     overlay.classList.add('active');
+});
+
+
+const openMenuCategory = document.getElementById('openMenuCategory');
+const closeMenuCategory = document.getElementById('closeMenuCategory');
+const sidebarMenuCategory = document.getElementById('sidebarMenuCategory');
+const menuLinksCategory = document.querySelectorAll('#sidebarMenuCategory a');
+
+const overlayCategory = document.getElementById('overlayCategory');
+
+menuLinksCategory.forEach(link => {
+    link.addEventListener('click', () => {
+        closeSidebarCategory();
+    });
+});
+
+const closeSidebarCategory = () => {
+    sidebarMenuCategory.classList.remove('active');
+    overlayCategory.classList.remove('active');
+};
+
+
+closeMenuCategory.addEventListener('click', closeSidebarCategory);
+overlayCategory.addEventListener('click', closeSidebarCategory);
+
+openMenuCategory.addEventListener('click', () => {
+    sidebarMenuCategory.classList.add('active');
+    overlayCategory.classList.add('active');
 });
